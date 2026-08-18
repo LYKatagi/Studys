254f98bc1d35c5f3fff98a53b54742f5bb3507fa
LYKatagi <rakatgi@yahoo.com.br>
2026-08-18 06:09:58 -0300

    rename: define PAR_OR_IMPAR to PAR_OU_IMPAR

--- src/c/exercises/01-basics/par_or_impar/include/par_ou_impar.h
+++
@@ -1,6 +0,0 @@
-#ifndef PAR_OR_IMPAR_H
-#define PAR_OR_IMPAR_H
-
-int is_even(int num);
-
-#endif
--- src/c/exercises/01-basics/par_ou_impar/par_ou_impar.c
+++ src/c/exercises/01-basics/par_ou_impar/par_ou_impar.c
@@ -1,6 +1,6 @@
 #include "include/par_ou_impar.h"
 
-int is_even(int num){
+int is_even(int number){
     // Implemente sua resolução aqui
 }
 
